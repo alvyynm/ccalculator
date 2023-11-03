6e8b4018eff2861c0aa2325a82b1f49c0f9aaf47
alvyynm <alvyynm@gmail.com>
2023-11-04 00:20:22 +0300

    add include guards

--- src/calculatorlib.h
+++ src/calculatorlib.h
@@ -1,6 +1,9 @@
 // FUNCTION PROTOTYPES TO USE IN .c files
+#ifndef HEADER_NAME_H
+#define HEADER_NAME_H
 int add(int a, int b);
 int subtract(int a, int b);
 int divide(int a, int b);
 int mod(int a, int b);
 int multiply(int a, int b);
+#endif
